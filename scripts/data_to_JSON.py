071e7a92cf3552a9dcff664b26f9ef578f6e21cf
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2020-05-03 09:49:19 +0200

    History goes back to March 2020 the 18th

--- scripts/data_to_JSON.py
+++ scripts/data_to_JSON.py
@@ -10,31 +10,47 @@ in CSV format to a JSON object.
 #
 import csv
 import json
+import datetime
 
 #
 #   User functions
 #
 def main():
 
+    # Now
+    date = datetime.datetime.now()
+    today = f'{date.year}-{date.month}-{date.day}'
+
     # Paths to the files
     path_to_geo = '../data/departements.geojson'
     path_to_data = '../data/donnees-hospitalieres.csv'
     path_to_geo_full = '../data/covid-france.json'
 
-    stats = {}
+    # Useful structures to analyse the data
+    accounts = dict()   # Dictionary with the accounts
+    days = set()        # All the days listed
+    departments = set() # All the departments listed
 
     # Reading the CSV file
     with open(path_to_data, newline='') as csvfile:
-        fieldnames = ['dep', 'sexe', 'jour', 'hosp', 'rea', 'rad', 'dc']
+        fieldnames = ['code', 'sex', 'day', 'hosp', 'rea', 'rad', 'dc']
         lines = csv.DictReader(csvfile, delimiter=';', fieldnames=fieldnames)
-        for line in lines:
-            if line['jour'] == '2020-04-30':
-                if line['sexe'] == '0':
-                    stats[line['dep']] = {'T': line['dc']}
-                if line['sexe'] == '1':
-                    stats[line['dep']].update({'M': line['dc']})
-                if line['sexe'] == '2':
-                    stats[line['dep']].update({'W': line['dc']})
+        for idx, line in enumerate(lines):
+            if idx != 0:
+                days.add(line['day'])
+                departments.add(line['code'])
+
+    for department in departments:
+        accounts.update({
+            department: { day: dict() for day in days }
+        })
+
+    with open(path_to_data, newline='') as csvfile:
+        fieldnames = ['code', 'sex', 'day', 'hosp', 'rea', 'rad', 'dc']
+        lines = csv.DictReader(csvfile, delimiter=';', fieldnames=fieldnames)
+        for idx, line in enumerate(lines):
+            if idx != 0:
+                accounts[line['code']][line['day']][line['sex']] = line['dc']
 
     # Reading the geoJSON file
     with open(path_to_geo) as geojson:
@@ -45,12 +61,13 @@ def main():
         for idx, department in enumerate(output['features']):
             code = department['properties']['code']
             department['properties'].update({
-                'deceased': stats.get(code)
+                'deceased': accounts.get(code)
                 })
             json.dump(department, jsonfile)
             if (idx + 1) < len(output['features']):
                 jsonfile.write(',\n')
         jsonfile.write('\n]}\n')
+
 #
 #   Main
 #

5e2e20daa850a011aebc63c0446385a01137ae69
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2020-05-06 16:06:23 +0200

    Better controls on the geoJSON layer

--- app/components/fr-covid-map.js
+++ app/components/fr-covid-map.js
@@ -16,7 +16,7 @@ let frCovidMapCmpnt = {
         /*
         *   Sets a color according to the value of a stat
         */
-        covidColor: function(n) {
+        covidColor(n) {
             return  n > 1000 ? 'hsl(180, 100%, 9%)' :
                     n > 750 ? 'hsl(180, 100%, 19%)' :
                     n > 500 ? 'hsl(180, 100%, 25%)' :
@@ -29,9 +29,24 @@ let frCovidMapCmpnt = {
                               'hsl(180, 100%, 90%)';
         },
         /*
+        *   Highlights a department
+        */
+        highlightFeature(event) {
+            // Sets a specific style
+            event.target.setStyle({
+                weight: 2,
+                color: 'hsl(21, 91%, 53%)',
+                fillColor: 'hsl(21, 91%, 73%)',
+            });
+
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                event.target.bringToFront();
+            }
+        },
+        /*
         *   Initializes a GeoJSON layer.
         */
-        initGeoJSON: function(date = null) {
+        initGeoJSON(date = null) {
             fetch('./data/covid-france.json')
             .then(stream => stream.json())
             .then(departments => {
@@ -41,7 +56,7 @@ let frCovidMapCmpnt = {
                 // GeoJSON layer
                 this.layer = L.geoJSON(departments, {
                     style: this.setStyle,
-                    onEachFeature: this.setPopup
+                    onEachFeature: this.onEachFeature
                 });
                 this.layer.addTo(this.map);
             });
@@ -49,9 +64,9 @@ let frCovidMapCmpnt = {
         /*
         *   Initializes an OpenStreetMap.
         */
-        initMap: function() {
+        initMap() {
             // Setting a new Leaflet instance
-            this.map = L.map('map').setView([47,2], 6);
+            this.map = L.map('map').setView([47, 2], 6);
             // OSM tiles
             L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: 'Données géographiques © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributeurs, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
@@ -59,9 +74,28 @@ let frCovidMapCmpnt = {
             }).addTo(this.map);
         },
         /*
+        *   Defines all the actions to set on a department
+        */
+        onEachFeature(feature, layer) {
+            // Place a popup
+            this.setPopup(feature, layer);
+            // Associates events with methods
+            layer.on({
+                click: this.zoomToFeature,
+                mouseout: this.resetHighlight,
+                mouseover: this.highlightFeature
+            });
+        },
+        /*
+        *   Resets the state of a feature
+        */
+        resetHighlight(event) {
+            this.layer.resetStyle(event.target);
+        },
+        /*
         *   Sets the popup on a particular department.
         */
-        setPopup: function (department, layer) {
+        setPopup(department, layer) {
             let date = this.dateToDisplay.format('YYYY-MM-DD');
             if (department.properties && department.properties.nom && department.properties.deceased) {
                 let content = `<h6>${department.properties.nom} (${department.properties.code})</h6>\
@@ -74,7 +108,7 @@ let frCovidMapCmpnt = {
         /*
         *   Defines the style properties of a department.
         */
-        setStyle: function(department) {
+        setStyle(department) {
             let date = this.dateToDisplay.format("YYYY-MM-DD");
             return {
                 fillColor: this.covidColor(department.properties.deceased[date][0]),
@@ -82,6 +116,9 @@ let frCovidMapCmpnt = {
                 color: 'white',
                 fillOpacity: 1
             };
+        },
+        zoomToFeature(event) {
+            this.map.fitBounds(event.target.getBounds());
         }
     }
 }
